3912782614ae3f6f6eea6b5cba5ce367ce3389ab
ESHER5517 <255517@esher.ac.uk>
2025-12-19 06:18:46 +0000

    -_-

--- testing_env/js/vault.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.accordion').forEach(btn=>{
- btn.addEventListener('click',()=>{
-  const expanded=btn.getAttribute('aria-expanded')==='true';
-  btn.setAttribute('aria-expanded',!expanded);
-  btn.nextElementSibling.style.display=expanded?'none':'block';
- });
-});
